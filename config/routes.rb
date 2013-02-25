777a335fa7fb6c056d74e118512e0d651ef70850
Jeff Hallett <jeff@mywaynetworks.com>
2013-02-25 00:13:11 -0800

    Fix Errors to JQuery Masonry

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ OmrailsX::Application.routes.draw do
 
   get 'about' => 'pages#about'
   
-  root :to => 'pages#home'
+  root :to => 'pins#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
